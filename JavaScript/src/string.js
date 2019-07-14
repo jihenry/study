32f147869cc2ca68e085947b3c372eb204c8d1b5
wtoeasy <wdoeasy@126.com>
2019-07-14 22:38:23 +0800

    搭建ts和js开发环境

---
+++ JavaScript/src/string.js
@@ -0,0 +1,18 @@
+var a = "dgagahaha";
+console.log("dffggggggggggg:",a)
+
+
+function Student({name="aaaaaa",age=12}) {
+    this.name = name;
+    console.log(name,age)
+
+    return 111;
+}
+
+var wang = new Student({age:"xxxxx"})
+
+Student({name:"gggg"})
+
+console.log("name:",name)
+
+console.log("student ",wang)
---
+++ TypeScript/dist/string.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log("工程搭建");
+//# sourceMappingURL=string.js.map
