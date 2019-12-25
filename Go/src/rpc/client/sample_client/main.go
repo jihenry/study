8df76ae8d3ac742baaebfe10a0bc79e923b7e02f
wdoeasy <wdoeasy@126.com>
2019-12-25 15:57:14 +0800

    rpc 证书

--- Go/src/rpc/client/sample_client/main.go
+++ Go/src/rpc/client/sample_client/main.go
@@ -4,21 +4,50 @@ import (
 	"context"
 	"log"
 
-	"google.golang.org/grpc"
-
 	pb "rpc/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 //PORT ...
 const PORT = "9001"
 
 func main() {
-	conn, err := grpc.Dial(":"+PORT, grpc.WithInsecure())
+	//1. 使用证书，第二个参数生成证书指定的Name
+	c, err := credentials.NewClientTLSFromFile("../../conf/server/server.pem", "go-grpc-viking")
+	if err != nil {
+		log.Fatalf("credentials.NewClientTLSFromFile err: %v", err)
+	}
+	conn, err := grpc.Dial(":"+PORT, grpc.WithTransportCredentials(c))
+	//2. 不使用证书
+	// conn, err := grpc.Dial(":"+PORT, grpc.WithInsecure())
+	//3. 使用CA颁发的证书
+	// cert, err := tls.LoadX509KeyPair("../../conf/client/client.pem", "../../conf/client/client.key")
+	// if err != nil {
+	// 	log.Fatalf("tls.LoadX509KeyPair err: %v", err)
+	// }
+	// certPool := x509.NewCertPool()
+	// ca, err := ioutil.ReadFile("../../conf/ca.pem")
+	// if err != nil {
+	// 	log.Fatalf("ioutil.ReadFile err: %v", err)
+	// }
+
+	// if ok := certPool.AppendCertsFromPEM(ca); !ok {
+	// 	log.Fatalf("certPool.AppendCertsFromPEM err")
+	// }
+
+	// c := credentials.NewTLS(&tls.Config{
+	// 	Certificates: []tls.Certificate{cert},
+	// 	ServerName:   "go-grpc-viking",
+	// 	RootCAs:      certPool,
+	// })
+	// conn, err := grpc.Dial(":"+PORT, grpc.WithTransportCredentials(c))
+
 	if err != nil {
 		log.Fatalf("grpc.Dial err: %v", err)
 	}
 	defer conn.Close()
-
 	client := pb.NewServiceSearchClient(conn)
 	resp, err := client.Search(context.Background(), &pb.SearchRequest{
 		Request: "gRPC",
--- Go/src/rpc/server/sample_server/main.go
+++ Go/src/rpc/server/sample_server/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net"
+	"os"
+	"path/filepath"
 
 	pb "rpc/proto"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 //SearchService ...
@@ -22,7 +26,43 @@ func (s *SearchService) Search(ctx context.Context, r *pb.SearchRequest) (*pb.Se
 const PORT = "9001"
 
 func main() {
-	server := grpc.NewServer()
+	//1. 使用证书
+	c, err := credentials.NewServerTLSFromFile("../../conf/server/server.pem", "../../conf/server/server.key")
+	fpath, _ := filepath.Abs("../../conf/server.pem")
+	fmt.Println(fpath)
+	fmt.Println(filepath.Abs(os.Args[0]))
+	if err != nil {
+		log.Fatalf("credentials.NewServerTLSFromFile err: %v", err)
+	}
+	server := grpc.NewServer(grpc.Creds(c))
+
+	//2. 不是用证书
+	// server := grpc.NewServer()
+
+	//3. 使用CA颁发的证书
+	// cert, err := tls.LoadX509KeyPair("../../conf/server/server.pem", "../../conf/server/server.key")
+	// if err != nil {
+	// 	log.Fatalf("tls.LoadX509KeyPair err: %v", err)
+	// }
+
+	// certPool := x509.NewCertPool()
+	// ca, err := ioutil.ReadFile("../../conf/ca.pem")
+	// if err != nil {
+	// 	log.Fatalf("ioutil.ReadFile err: %v", err)
+	// }
+
+	// if ok := certPool.AppendCertsFromPEM(ca); !ok {
+	// 	log.Fatalf("certPool.AppendCertsFromPEM err")
+	// }
+
+	// c := credentials.NewTLS(&tls.Config{
+	// 	Certificates: []tls.Certificate{cert},
+	// 	ClientAuth:   tls.RequireAndVerifyClientCert,
+	// 	ClientCAs:    certPool,
+	// })
+
+	// server := grpc.NewServer(grpc.Creds(c))
+
 	pb.RegisterServiceSearchServer(server, &SearchService{})
 	lis, err := net.Listen("tcp", ":"+PORT)
 	if err != nil {
