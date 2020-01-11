2bd78dacd0c2667262e1a0c6264e0e7b4e8a53d2
wtoeasy <wdoeasy@126.com>
2020-01-11 15:07:45 +0800

    添加go.mod

---
+++ Go/src/lang/channel/channel.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func f1(a chan int) {
+	defer wg.Done()
+	for index := 0; index < 100; index++ {
+		a <- index
+	}
+	close(a)
+}
+
+func f2(a chan int, b chan int) {
+	defer wg.Done()
+	for x := range a {
+		b <- x * x
+	}
+	close(b)
+}
+
+func main() {
+	wg.Add(2)
+	ch1 := make(chan int, 10)
+	ch2 := make(chan int, 10)
+	go f1(ch1)
+	go f2(ch1, ch2)
+	for x := range ch2 {
+		fmt.Println(x)
+	}
+}
---
+++ Go/src/thirdlib/uuid/go.mod
@@ -0,0 +1,15 @@
+module uuidmodule
+
+go 1.12
+
+require (
+	github.com/chilts/sid v0.0.0-20190607042430-660e94789ec9
+	github.com/kjk/betterguid v0.0.0-20170621091430-c442874ba63a
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/oklog/ulid v1.3.1
+	github.com/rs/xid v1.2.1
+	github.com/satori/go.uuid v1.2.0
+	github.com/segmentio/ksuid v1.0.2
+	github.com/sony/sonyflake v1.0.0
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+)
